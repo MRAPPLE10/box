3c39dfec2983c29bb33a25d1f8f3104bd5cb5da2
MRAPPLE10 <65347112+MRAPPLE10@users.noreply.github.com>
2020-06-05 19:59:11 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class ground {
+    constructor(){
+     var options = {
+      isStatic : true
+     }
+     this.body = Bodies.rectangle(200,380,400,20,options);
+     this.width = 400;
+     this.height = 20;
+     World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+    fill(255);
+    rect(pos.x, pos.y, this.width, this.height);
+    
+    }
+}
